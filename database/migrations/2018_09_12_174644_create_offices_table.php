b62018f0935c3c83968ad5aa40fc9d55fcb00eb9
Carol Mirabal <carolmirabal27@gmail.com>
2018-09-12 16:36:47 -0300

    base de datos migracion

--- database/migrations/2018_09_12_174644_create_offices_table.php
+++ database/migrations/2018_09_12_174644_create_offices_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateoOfficesTable extends Migration
+class CreateOfficesTable extends Migration
 {
     /**
      * Run the migrations.
