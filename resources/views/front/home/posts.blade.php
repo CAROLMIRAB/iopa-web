f7a1885086fa52ce31340c7cdbf86f31458300c2
Carol Mirabal <carolmirabal27@gmail.com>
2018-10-02 18:11:07 -0300

    posts

--- resources/views/front/home/posts.blade.php
+++ resources/views/front/home/posts.blade.php
@@ -1,5 +1,5 @@
 @foreach ($posts as $post)
-<div class="col-sm-4">
+    <div class="col-sm-4">
         <div class="ui-post-preview">
             <figure>
                 <a href="#!">
@@ -18,40 +18,3 @@
         </div>
     </div>
 @endforeach
-    <!--<div class="col-sm-4">
-            <div class="ui-post-preview">
-                <figure>
-                    <a href="#!">
-                        <img src="{{ asset('/img/post-preview02.jpg') }}" alt="">
-                    </a>
-                </figure>
-                <div class="ui-preview-content">
-                    <a href="#!" class="ui-preview-title">
-                        Mírame a los ojos y cuéntame
-                    </a>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing
-                        eli.
-                    </p>
-                </div>
-                <a href="#!" class="btn ui-more">Ver +</a>
-            </div>
-        </div>
-
-        <div class="col-sm-4">
-            <div class="ui-post-preview">
-                <figure>
-                    <a href="#!">
-                        <img src="{{ asset('/img/post-preview03.jpg') }}" alt="">
-                    </a>
-                </figure>
-                <div class="ui-preview-content">
-                    <a href="#!" class="ui-preview-title">
-                        Ver mejor día a día
-                    </a>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing
-                        eli.
-                    </p>
-                </div>
-                <a href="#!" class="btn ui-more">Ver +</a>
-            </div>
-        </div>    -->
