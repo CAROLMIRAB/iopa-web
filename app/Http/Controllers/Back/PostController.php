a34ae93427c0ff84bd577b914ac1ae5d4dfddc94
Carol Mirabal <carolmirabal27@gmail.com>
2019-03-18 17:55:44 -0300

    base64

--- app/Http/Controllers/Back/PostController.php
+++ app/Http/Controllers/Back/PostController.php
@@ -130,8 +130,8 @@ class PostController extends Controller
             $image_url = "";
             $tags = explode(",", $request->tags);
 
-            if ($request->file('image')) {
-                $image_url = Core::uploadImage($request->file('image'));
+            if ($request->imgBase64) {
+                $image_url = Core::uploadImageB64($request->imgBase64);
             }
 
             $data = array(
