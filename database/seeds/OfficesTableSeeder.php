3c1454a276d63dcbfb16cbc1801b4423aa70e373
Carol Mirabal <carolmirabal27@gmail.com>
2019-03-19 14:55:13 -0300

    image office

--- database/seeds/OfficesTableSeeder.php
+++ database/seeds/OfficesTableSeeder.php
@@ -18,11 +18,11 @@ class OfficesTableSeeder extends Seeder
     {
         $now = now();
         $offices = [
-            ['Providencia', 'Av. Los Leones 391', '228760900', 'providencia'],
-            ['La Florida', 'Av. Vicuña Mackenna Oriente 6969, Piso 1', '226784800', 'la-florida'],
-            ['Santiago Centro', 'Paseo Huérfanos 1147 Piso 3, Of. 346', '223727060', 'santiago-centro'],
-            ['Buin', 'Santa María 201, Piso 2 Of. 201', '228760975', 'buin'],
-            ['Maipú', 'Av. Los Pajaritos 3195, Piso 16 Of. 1606', '228760980','maipu']
+            ['Providencia', 'Av. Los Leones 391', '228760900', 'providencia', 'no-foto.png'],
+            ['La Florida', 'Av. Vicuña Mackenna Oriente 6969, Piso 1', '226784800', 'la-florida', 'no-foto.png'],
+            ['Santiago Centro', 'Paseo Huérfanos 1147 Piso 3, Of. 346', '223727060', 'santiago-centro', 'no-foto.png'],
+            ['Buin', 'Santa María 201, Piso 2 Of. 201', '228760975', 'buin', 'no-foto.png'],
+            ['Maipú', 'Av. Los Pajaritos 3195, Piso 16 Of. 1606', '228760980','maipu', 'no-foto.png']
         ];
         $offices = array_map(function ($office) use ($now) {
             return [
@@ -30,8 +30,9 @@ class OfficesTableSeeder extends Seeder
                 'address' => $office[1],
                 'phone' => $office[2],
                 'slug' => $office[3],
+                'photo' => $office[4],
                 'updated_at' => $now,
-                'created_at' => $now,
+                'created_at' => $now
             ];
         }, $offices);
         \DB::table('offices')->insert($offices);
