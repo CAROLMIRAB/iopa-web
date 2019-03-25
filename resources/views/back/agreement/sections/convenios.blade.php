643b55ac5dc3cb886794589658327b88cff6c619
Carol Mirabal <carolmirabal27@gmail.com>
2019-03-25 18:09:03 -0300

    pagos

--- resources/views/back/agreement/sections/convenios.blade.php
+++ resources/views/back/agreement/sections/convenios.blade.php
@@ -57,7 +57,7 @@
             <h2>Logos Convenios</h2>
         </div>
         <div class="col-12">
-            <ul class="clearfix" id="sortable" class="sortable conv-sort">
+            <ul id="sortable" class="sortable conv-sort">
                 @if(!empty($convenio['content'])) @foreach($convenio['content'] as $key => $item)
                 <li data-img="{{ $item['img'] }}">
                     <div class="box-image nostatus">
