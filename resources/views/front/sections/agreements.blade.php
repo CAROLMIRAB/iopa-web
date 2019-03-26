381c2063790a3d65a65591bb5d320c8422c0e8e5
Carol Mirabal <carolmirabal27@gmail.com>
2019-03-26 15:47:47 -0300

    pagos web

--- resources/views/front/sections/agreements.blade.php
+++ resources/views/front/sections/agreements.blade.php
@@ -22,18 +22,30 @@
       <div class="col-md-9">
         <div class="ui-blog-body">
           <div class="ui-blog-content mt-10 mb-10">
-            @include('front.sections.agreesections.fonasa', ['fonasa' => isset($agreement[0]['fonasa']) ? $agreement[0]['fonasa'] : ''])
+
+            @if($agreement[0]['fonasa']['status'] == 'ACTIVE')
+  @include('front.sections.agreesections.fonasa', ['fonasa' => isset($agreement[0]['fonasa'])
+            ? $agreement[0]['fonasa'] : '']) @endif
             
-            @include('front.sections.agreesections.isapre', ['isapre' => isset($agreement[1]['isapres']) ? $agreement[1]['isapres']
-            : ''])
-            @include('front.sections.agreesections.convenios', ['convenio' => isset($agreement[2]['convenios']) ? $agreement[2]['convenios']
-            : '' ])
-            @include('front.sections.agreesections.promociones', ['promo' => isset($agreement[3]['promociones']) ? $agreement[3]['promociones']
-            : '' ])
-            @include('front.sections.agreesections.aranceles', ['arancel' => isset($agreement[4]['aranceles']) ? $agreement[4]['aranceles']
-            : '' ])
-            @include('front.sections.agreesections.pago', ['pago' => isset($agreement[5]['medios-pagos']) ? $agreement[5]['medios-pagos']
-            : '' ])
+            @if($agreement[0]['isapres']['status'] == 'ACTIVE')
+  @include('front.sections.agreesections.isapre',
+            ['isapre' => isset($agreement[1]['isapres']) ? $agreement[1]['isapres'] : '']) @endif 
+            
+            @if($agreement[0]['convenios']['status']== 'ACTIVE')
+  @include('front.sections.agreesections.convenios', ['convenio' => isset($agreement[2]['convenios']) 
+            ? $agreement[2]['convenios'] : '' ]) @endif 
+            
+            @if($agreement[0]['promociones']['status'] == 'ACTIVE')
+  @include('front.sections.agreesections.promociones',
+            ['promo' => isset($agreement[3]['promociones']) ? $agreement[3]['promociones'] : '' ]) @endif
+
+            @if($agreement[0]['aranceles']['status'] == 'ACTIVE')
+  @include('front.sections.agreesections.aranceles',
+            ['arancel' => isset($agreement[4]['aranceles']) ? $agreement[4]['aranceles'] : '' ]) @endif
+
+            @if($agreement[0]['medios-pagos']['status'] == 'ACTIVE')
+  @include('front.sections.agreesections.pago',
+            ['pago' => isset($agreement[5]['medios-pagos']) ? $agreement[5]['medios-pagos'] : '' ]) @endif
 
           </div>
         </div>
