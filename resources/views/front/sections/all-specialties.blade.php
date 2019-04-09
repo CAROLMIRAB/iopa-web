f4084072f30afb1c1b3a372654094e0c3559e7ef
Carol Mirabal <carolmirabal27@gmail.com>
2019-04-09 19:08:34 -0300

    cambio

--- resources/views/front/sections/all-specialties.blade.php
+++ resources/views/front/sections/all-specialties.blade.php
@@ -14,46 +14,18 @@
       </div>
 
       <div class="row row-especialidades">
+          @foreach ($specialties as $specialty)
           <div class="col-sm-3">
             <div class="item-especialidad ">
               <a href="#!">
                 <img src="./assets/img/home/papilas.jpg" alt="Fotos de Papilas">
-                <h5>Fotos de Papilas</h5>
+                <h5>{{ $specialty->name }}</h5>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
                 <span class="ui-go">Ver más</span>
               </a>
             </div>
           </div>
-          <div class="col-sm-3">
-            <div class="item-especialidad " data-wow-delay=".3s">
-              <a href="#!">
-                <img src="./assets/img/home/diloscopia.jpg" alt="Diploscopia">
-                <h5>Diploscopia</h5>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                <span class="ui-go">Ver más</span>
-              </a>
-            </div>
-          </div>
-          <div class="col-sm-3">
-            <div class="item-especialidad " data-wow-delay=".6s">
-              <a href="#!">
-                <img src="./assets/img/home/radiografia.jpg" alt="Radiografía">
-                <h5>Radiografía</h5>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                <span class="ui-go">Ver más</span>
-              </a>
-            </div>
-          </div>
-          <div class="col-sm-3">
-            <div class="item-especialidad" data-wow-delay=".9s">
-              <a href="#!">
-                <img src="./assets/img/home/nuestros.jpg" alt="Médicos">
-                <h5>Nuestros Médicos</h5>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                <span class="ui-go">Ver más</span>
-              </a>
-            </div>
-          </div>
+          @endforeach
         </div>
 
     </div>
