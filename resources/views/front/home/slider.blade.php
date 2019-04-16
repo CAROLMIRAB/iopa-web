ce82c5d11ab8f61c27aac343967b7821888a0362
Carol Mirabal <carolmirabal27@gmail.com>
2019-04-16 16:49:35 -0300

    admin

--- resources/views/front/home/slider.blade.php
+++ resources/views/front/home/slider.blade.php
@@ -45,7 +45,7 @@
 	</div>
 </section>
 
-@if(isset($config[4]['popup']) )
+@if(isset($config[4]['popup']['content']) )
 @if($config[4]['popup']['content']['status'] == 'PUBLISHED')
 <div class="modal fade modal-sucursal" id="popup-principal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
 	<div class="modal-dialog modal-lg" role="document">
