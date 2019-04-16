fd53789074b4fed47e9fd6b577b60d17994616f1
Carol Mirabal <carolmirabal27@gmail.com>
2019-04-16 17:47:51 -0300

    admin

--- resources/views/back/theme.blade.php
+++ resources/views/back/theme.blade.php
@@ -3,6 +3,7 @@
 
 <head>
   <meta charset="utf-8">
+  <link rel="icon" type="image/png" href="{{ asset('images/favicon.ico') }}">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
   <meta name="author" content="Creative Tim">
